aec8eeddf3b117e0d9f336bf4a8653b3081dd305
Lucky Lebeurre <luckylebeurre@MacBook-Pro-de-Lucky.local>
2025-05-28 09:17:45 +0200

    mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,10 @@ export default function RootLayout({ children }: RootLayoutProps) {
     <html lang="fr">
       <head>
         {/* Pour que la barre de statut soit transparente sur mobile */}
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="theme-color" content="#000000" />
       </head>
       <body>
         {children}
